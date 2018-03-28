4f97e3a82135c63412a576d7171b048d53fb5ea9
Ryan Patel <rajanpatel1011@gmail.com>
2018-03-28 21:18:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,44 @@
 //define functions here
+function getIt(){
+  $('p').on("click",function(){
+    alert("Hey!");
+  });
+}
+function frameIt(){
+  $('body').on("load",function(){
+    ('img').addClass("tasty");
+  });
+}
+function PressIt(){
+  $('document').on("keydown",function(key){
+    if(key.which=='g'){
+     alert("pressed G!");
+    }
+    if(key.which!='g'){
+      alert("not G key pressed");
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on("submit",function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+
+
+
+getIt();
+frameIt();
+PressIt();
+submitIt();
+
+
+
 
 });
